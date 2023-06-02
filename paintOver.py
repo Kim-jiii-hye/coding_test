0f429e0476871b20e4d3e17f927b6c9509972574
JIHYE KIM <79744589+Kim-jiii-hye@users.noreply.github.com>
2023-06-02 11:46:38 +0900

    #5 paint over

--- paintOver.py
+++ paintOver.py
@@ -1,28 +1,27 @@
 # 덧칠하기
-# n=>8(총 칠해야할 칸) m=>4(롤러의 길이) section=>[2,3,6](처음 칠하는 곳)
-# result 2
-# 2023-06-02 start 10:05 - 10:43
-# 채점 : 34.0/100.0
+# n=>4(총 칸 개수) m=>1(롤러의 길이) section=>[1,2,3,4](처음 칠하는 곳)
+# result 4
+# 2023-06-02 start 10:48 - 11:30
+# 채점 : 18.0/100.0
 
 def solution(n, m, section):
     answer = 0
+    # status = []
+    # 2, 3, 4, 5
+    # 3, 4, 5, 6
+    # 만약 최대 값이 section 의 최대값과 같다면 그만
+    if(len(section) <= n):
+        for i in section:
+            if ((i-1) + m <= n):
+                answer = answer + 1
+            # else:
 
-    # # [false, false, false, false, false, false, false, false]
-    status = [False for i in range(n)]
-    # # [false, true, true, false, false, true, false, false]
-    for ch in section:
-        status[ch-1] = True
-    # print(status)
 
-    # true 가 있는 영역에 페인트 다시 칠하기
-    for idx, st in enumerate(status):
-        if(st == True):
-            for i in range(m-1):
-                status[i+1] = False
-            answer = answer+1
+            # print(status)
+        print(answer)
 
-    print(answer)
     return answer
 
 if __name__ == '__main__':
     solution(8, 4, [2, 3, 6])
+    solution(4, 1, [1, 2, 3, 4])
