f0bd337bc754b3d15762d449bd52f66a99e82f15
JIHYE KIM <79744589+Kim-jiii-hye@users.noreply.github.com>
2023-06-21 23:04:36 +0900

    solved

--- fruitSeller.py
+++ fruitSeller.py
@@ -1,30 +1,34 @@
 # 과일 장수
 
-# 2023-06-20 start 22:00 - stop
+# 2023-06-20 start 22:00 - stop 23:35
+# 2023-06-21 start 22:00 - stop 23:04
 
 # 채점 : 100.0/100.0
-from itertools import permutations
 def solution(k, m, score):
-    box_res = []
-
     # 최솟값보다 큰 값
     box = []
-    for i in score:
-        if len(box) < m:
-            if min(score) < i:
-                box.append(i)
+    sort_score = sorted(score, reverse=True)
 
-        if box not in box_res:
-            box_res.append(box)
+    for i in range(0, len(sort_score), m):
 
-    # print(box_res)
-    min_app = min(box_res[0])
-    one_box = m
-    box_cnt = len(box_res)
-    res = min_app * one_box * box_cnt
-    print(res)
+        arr = sort_score[i:i + m]
+        box.append(arr)
+
+    print(box)
+    answer = 0
+
+    for j in box:
+        if len(j) == m:
+            min_ap = min(j)
+            # print(f"{min_ap} * {m}")
+            res = min_ap * m
+            answer = answer + res
+    print(answer)
+
+
+
 
 if __name__ == '__main__':
     solution(3, 4, [1,2,3,1,2,3,1])
-    # solution(4, 3, [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2])
+    solution(4, 3, [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2])
 
