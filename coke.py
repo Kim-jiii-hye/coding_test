dbbcd955e86db5b8789a611cad8a2ebf6463dcfb
JIHYE KIM <79744589+Kim-jiii-hye@users.noreply.github.com>
2023-06-30 07:45:11 +0900

    not solved

--- coke.py
+++ coke.py
@@ -1,20 +1,19 @@
 # 콜라 문제
 import math
 # 2023-06-29 start 07:10 -
+# 2023-06-30 start 07:10 -
 
 # 채점 : 0.0/100.0
 def solution(a, b, n):
     # 빈병 20개 가져감 => 20/2 10병 모두 마심 => 10/2 5병 모두 => 5/2 2병 => 2병 /2 1병 => 남아있던 1병 까지
-    # print(f"{n} - {for_mart} + {get_coke} = {have_coke}")
-    # print(f"{for_mart} + {have_coke} - {n} = {get_coke}")
+    get_coke = int((n / a) * b)
+    print(get_coke)
+    while(get_coke > 0):
+        get_coke = round((get_coke / a) * b)
+        print(get_coke)
 
-    get_coke = int(n / a)
-    print(get_coke)
-    get_coke = int(get_coke / a)
-    print(get_coke)
-    get_coke = int(get_coke / a)
-    print(get_coke)
+    print()
 
 if __name__ == '__main__':
-    # solution(2, 1, 20)
+    solution(2, 1, 20)
     solution(3, 1, 20)
