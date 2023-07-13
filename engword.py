70813416239c06648458ef3a667ff96389a61e27
JIHYE KIM <79744589+Kim-jiii-hye@users.noreply.github.com>
2023-07-14 07:37:16 +0900

    solved

--- engword.py
+++ engword.py
@@ -1,22 +1,32 @@
 # 숫자 문자열과 영단어
 
 # 2023-07-12 start 08:33 -
+# 2023-07-13 start 07:10 - 07:37
 
-# 채점 : 0/100.0
+# 채점 : 100.0/100.0
 def solution(s):
-
     data = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    print(s)
     check = ''
+    answer = s
     for (idx, i) in enumerate(s):
-        if check.isdigit() == False:
-            check = check + i
-            print(check)
+        if not i.isdigit():
+            if check not in data:
+                check = check + i
+            if check in data:
+                # print(check)
+                # print(data.index(check))
+                answer = answer.replace(check, str(data.index(check)))
+                check = ''
+    print(answer)
+    return int(answer)
 
-        if check in data:
-            print(idx)
-            print(check)
-            check = ''
+
+
+
+        # if check in data:
+        #     print(idx)
+        #     print(check)
+        #     check = ''
 
 
 if __name__ == '__main__':
